a256adbe42f852175a1ba10de1cf58381cbed3d4
0xcacti <0xcacti@gmail.com>
2026-01-17 23:21:38 -0500

    aoeu

--- pwd/main.c
+++ pwd/main.c
@@ -1,18 +1,28 @@
 #define _POSIX_C_SOURCE 200809L
 
 #include <errno.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
+static void usage(const char *progname) {
+  dprintf(STDERR_FILENO, "usage: %s [-L | -P]\n", progname);
+}
+
 int main(int argc, char *argv[]) {
   int ch;
 
-  while ((ch = getopt(argc, argv, "")) != -1) {
+  bool logical = true;
+  bool physical = false;
+
+  while ((ch = getopt(argc, argv, "L:P:")) != -1) {
     switch (ch) {
+    case 'L':
+      break;
     default:
-      error_errno(argv[0], "bad option");
+      usage(argv[0]);
       return 1;
     }
   }
