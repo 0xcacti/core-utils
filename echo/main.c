291bb0cd47ad1e81035eafe3d2f437adfc10aa95
0xcacti <0xcacti@gmail.com>
2026-01-23 02:23:55 -0500

    it's currently wrong, but I gotta go bedtime

--- echo/main.c
+++ echo/main.c
@@ -1,4 +1,5 @@
 #include <errno.h>
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,20 +16,24 @@ static void error_msg(const char *progname, const char *msg) {
 int main(int argc, char *argv[]) {
   const char *progname = argv[0];
 
-  if (argc < 2) {
-    error_msg(progname, "missing operand");
-    return EXIT_FAILURE;
-  }
+  int suppress_newline = 0;
 
-  int exit_status = EXIT_SUCCESS;
-
-  for (int i = 1; i < argc; i++) {
-    const char *filename = argv[i];
-    if (unlink(filename) != 0) {
-      error_errno(progname, filename);
-      exit_status = EXIT_FAILURE;
+  int ch;
+  while ((ch = getopt(argc, argv, "n")) != -1) {
+    switch (ch) {
+    case 'n':
+      suppress_newline = 1;
+      break;
     }
   }
 
-  return exit_status;
+  char *output = argv[optind];
+  if (output == NULL) return 0;
+
+  if (suppress_newline) {
+    printf("%s", output);
+  } else {
+    printf("%s\n", output);
+  }
+  return 0;
 }
