063bafea96c714355b2b96f9587e49fbd4013bac
0xcacti <0xcacti@gmail.com>
2026-01-31 01:24:40 -0500

    okay keepy working on the clintymoo

--- touch/main.c
+++ touch/main.c
@@ -1,11 +1,15 @@
+#include <errno.h>
+#include <fcntl.h>
 #include <getopt.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/stat.h>
 
 static void usage(const char *progname) {
   dprintf(STDERR_FILENO, "Usage: %s [-achm] [-t [[CC]YY]MMDDhhmm[.SS]] [-r file] file ...\n",
           progname);
+  exit(2);
 }
 
 int main(int argc, char *argv[]) {
@@ -40,6 +44,43 @@ int main(int argc, char *argv[]) {
       break;
     default:
       usage(argv[0]);
+    }
+  }
+
+  // when not specified, default to -a and -m
+  if (!opt_a && !opt_m) {
+    opt_a = true;
+    opt_m = true;
+  }
+
+  while (optind != argc) {
+    printf("Filename: %s\n", argv[optind]);
+    char *path = argv[optind];
+    bool exists = true;
+    struct stat st;
+    int ret = stat(path, &st);
+    if (ret != 0) {
+      if (errno == ENOENT) {
+        exists = false;
+      } else {
+        perror("stat");
+        return 1;
+      }
+    }
+
+    if (exists) {
+      // donkey
+    } else if (!exists && opt_c) {
+      // don't create
+    } else {
+      // create
+    }
+
+    // if exists
+    // handle c
+    int s = open(argv[optind++], O_CREAT | O_WRONLY, 0666);
+    if (s == -1) {
+      perror("open");
       return 1;
     }
   }
