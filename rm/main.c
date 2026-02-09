72b122cf6358cb859ccffcd4b57cde18cb18453d
0xcacti <0xcacti@gmail.com>
2026-02-08 22:27:08 -0500

    clean up comments

--- rm/main.c
+++ rm/main.c
@@ -1,35 +1,3 @@
-// SYNOPSIS
-//      rm [-f | -i] [-dIRrvWx] file ...
-//      unlink [--] file
-//
-// DESCRIPTION
-//      The rm utility attempts to remove the non-directory type files specified on the command
-//      line.  If the permissions of the file do not permit writing, and the standard input device
-//      is a terminal, the user is prompted (on the standard error output) for confirmation.
-//
-//      The options are as follows:
-//
-//      -d      Attempt to remove directories as well as other types of files.
-//
-//      -f      Attempt to remove the files without prompting for confirmation, regardless of the
-//      file's permissions.  If the file does not exist, do not display a
-//              diagnostic message or modify the exit status to reflect an error.  The -f option
-//              overrides any previous -i options.
-//
-//      -i      Request confirmation before attempting to remove each file, regardless of the file's
-//      permissions, or whether or not the standard input device is a
-//              terminal.  The -i option overrides any previous -f options.
-//
-//      -r      Attempt to remove the file hierarchy rooted in each file argument.  The -R option
-//      implies the -d option.  If the -i option is specified, the user is
-//              prompted for confirmation before each directory's contents are processed (as well as
-//              before the attempt is made to remove the directory).  If the user does not respond
-//              affirmatively, the file hierarchy rooted in that directory is skipped.
-//
-//      -v      Be verbose when deleting files, showing them as they are removed.
-//
-//      The rm utility removes symbolic links, not the files referenced by the links.
-//      It is an error to attempt to remove the files /, . or ...
 #include <errno.h>
 #include <getopt.h>
 #include <stdbool.h>
