0930314268981ebfaf9a52d57b70798c09f4316d
0xcacti <0xcacti@gmail.com>
2026-03-13 02:07:17 -0400

    CORE THING IS WORKINGgit add -A git

--- du/main.c
+++ du/main.c
@@ -62,7 +62,7 @@ static int dirsum_stack_push(dirsum_stack_t *ds, dirsum_t d) {
 
 static dirsum_t dirsum_stack_pop(dirsum_stack_t *ds) {
   if (ds->len == 0) return (dirsum_t){.s = -1, .active = false};
-  return ds->data[ds->len--];
+  return ds->data[--ds->len];
 }
 
 static dirsum_t dirsum_stack_peek(dirsum_stack_t *ds) {
@@ -128,6 +128,10 @@ static int du_path(char *path, flags_t flags) {
       break;
     case FTS_D: {
       long long sz = simple_block_size(ent->fts_path);
+      // for (int i = 0; i < ent->fts_level; i++) {
+      //   printf("\t");
+      // }
+      // printf("entering dir: %s\n", ent->fts_path);
       if (sz < 0) {
         int saved = errno;
         dirsum_stack_free(&ds);
@@ -146,6 +150,8 @@ static int du_path(char *path, flags_t flags) {
       break;
     }
     case FTS_DP: {
+      // printf("ds.len %zu\n", ds.len);
+      // printf("top of stack: %lld\n", ds.data[ds.len - 1].s);
       dirsum_t curr = dirsum_stack_pop(&ds);
       if (curr.s < 0) {
         int saved = errno;
@@ -155,11 +161,17 @@ static int du_path(char *path, flags_t flags) {
         return -1;
       }
       fprintf(stdout, "%lld %s\n", curr.s, ent->fts_path);
+      // printf("top of stack: %lld\n", ds.data[ds.len - 1].s);
       dirsum_stack_top_sum(&ds, curr.s);
       break;
     }
     case FTS_F: {
+
       long long sz = simple_block_size(ent->fts_path);
+      // for (int i = 0; i < ent->fts_level; i++) {
+      //   printf("\t");
+      // }
+      // printf("%s :: %lld\n", ent->fts_path, sz);
       if (sz < 0) {
         int saved = errno;
         dirsum_stack_free(&ds);
@@ -172,12 +184,15 @@ static int du_path(char *path, flags_t flags) {
           fprintf(stdout, "%lld %s\n", sz, ent->fts_path);
         }
       } else {
+
+        // printf("st before: %lld\n", ds.data[ds.len - 1].s);
         dirsum_stack_top_sum(&ds, sz);
+        // printf("st after : %lld\n", ds.data[ds.len - 1].s);
       }
       break;
     }
     default:
-      fprintf(stderr, "fts_info=%d path=%s\n", ent->fts_info, ent->fts_path);
+      printf("W are in the hole zone\n");
     }
   }
   fts_close(fts);
