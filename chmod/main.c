2fc35bab63f042cbaaae813e11bbf5a8ef4369cb
0xcacti <0xcacti@gmail.com>
2026-04-11 18:54:32 -0400

    start parsing mode

--- chmod/main.c
+++ chmod/main.c
@@ -1,7 +1,9 @@
+#include <errno.h>
 #include <getopt.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
@@ -30,9 +32,34 @@ static void usage(const char *progname) {
   exit(2);
 }
 
-// static mode_form_e parse_mode(const char *mode) {
-//   return MODE_OCTAL;
-// }
+static void error_errno(const char *progname, const char *filename) {
+  dprintf(STDERR_FILENO, "%s: %s: %s\n", progname, filename, strerror(errno));
+}
+
+static void error_msg(const char *progname, const char *m1, const char *m2) {
+  dprintf(STDERR_FILENO, "%s: %s: %s\n", progname, m1, m2);
+}
+
+static mode_form_e parse_mode(const char *mode) {
+  switch (mode[0]) {
+  case 'u':
+  case 'g':
+  case 'a':
+  case 'o':
+    return MODE_SYMBOLIC;
+  case '0':
+  case '1':
+  case '2':
+  case '3':
+  case '4':
+  case '5':
+  case '6':
+  case '7':
+    return MODE_OCTAL;
+  default:
+    return MODE_BAD;
+  }
+}
 
 // static void parse_octal(const char *mode) {
 //   return;
@@ -70,7 +97,16 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  //   mode_form_e mode = parse_mode(argv[optind]);
+  mode_form_e mode = parse_mode(argv[optind]);
+
+  switch (mode) {
+  case MODE_OCTAL:
+    break;
+  case MODE_SYMBOLIC:
+    break;
+  case MODE_BAD:
+    break;
+  }
 
   return 0;
 }
